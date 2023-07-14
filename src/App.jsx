22691b03aa343c476fe9010054014c2a2e21ca28
nathbalica <nathbalica@gmail.com>
2023-07-14 19:02:53 -0300

    feat: :sparkles: complete sign and sign up routes plus static home
    rotas completas de signIn e SignUp integradas com backend e rota home estatica e sem integração com backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import styled from "styled-components"
 import { AuthContextProvider } from "./contexts/AuthContext"
+import SignInPage from "./pages/SignInPage"
+import SignUpPage from "./pages/SignUpPage"
+import HomePage from "./pages/HomePage"
 
 
 export default function App() {
-
+  
   return (
     <AuthContextProvider>
       <PagesContainer>
         <BrowserRouter>
           <Routes>
-            {/* <Route path="/" element={<SignInPage />} />
-            <Route path="/cadastro" element={<SignUpPage />} />
+            <Route path="/" element={<SignInPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
             <Route path="/home" element={<HomePage />} />
-            <Route path="/nova-transacao/:type" element={<TransactionsPage />} />
+            {/* <Route path="/nova-transacao/:type" element={<TransactionsPage />} />
             <Route path="/editar-registro/:type/:id" element={<EditTransactionPage />} /> */}
       
           </Routes>
@@ -24,8 +27,7 @@ export default function App() {
 }
 
 const PagesContainer = styled.main`
-  background-color: #8c11be;
-  width: calc(100vw - 50px);
+  max-width: 100vw;
   max-height: 100vh;
-  padding: 25px;
+
 `
